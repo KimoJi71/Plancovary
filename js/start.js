0a8766817990d945006a53724d45daff4bc6d1c3
KimoJi71 <t107ab0048@ntut.org.tw>
2022-05-02 15:18:47 +0800

    reset

--- js/start.js
+++ js/start.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
     $("#start").hide().fadeIn(2000);
 });
 
-var userAgentInfo = navigator.userAgent; // 判斷手機版或電腦版
-
 // 按鈕特效轉換
 $("#start").bind("touchstart mouseover", function() {
     $("#start").attr("src", "../assets/Start/buttonAfter.png");
@@ -17,17 +15,7 @@ $("#start").bind("touchend mouseout", function() {
 });
 
 // 按鈕點擊後
-if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(userAgentInfo)) {
-    const start = document.getElementById("start");
-    start.addEventListener("click", e => {
-        e.preventDefault();
-        music.play();
-        localStorage.setItem("music_time", music.currentTime);
-        $(location).attr("href", "./slip.html");
-    });
-} else {
-    $("#start").click(function() {
-        localStorage.setItem("music_time", music.currentTime);
-        $(location).attr("href", "./slip.html");
-    });
-}
+$("#start").click(function() {
+    localStorage.setItem("music_time", music.currentTime);
+    $(location).attr("href", "./slip.html");
+});
